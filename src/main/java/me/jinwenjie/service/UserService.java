067f26fa32626c5db618f86b8db66cff766ec0ba
wenjie <jinwenjie@live.com>
2020-04-23 10:48:34 +0800

    Revert "refactor(user): remove interface"
    This reverts commit db9457582056ead38abb990c0b2086e48f2f271e.

--- src/main/java/me/jinwenjie/service/UserService.java
+++ src/main/java/me/jinwenjie/service/UserService.java
@@ -1,38 +1,17 @@
 package me.jinwenjie.service;
 
-import me.jinwenjie.dao.UserDao;
 import me.jinwenjie.model.User;
-import me.jinwenjie.model.UserExample;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class UserService {
-    private final UserDao userDao;
+public interface UserService {
+    List<User> list();
 
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
+    User get(Long id);
 
-    public List<User> list() {
-        UserExample example = new UserExample();
-        return userDao.selectByExample(example);
-    }
+    boolean create(User user);
 
-    public User get(Long id) {
-        return userDao.selectByPrimaryKey(id);
-    }
+    boolean update(User user);
 
-    public boolean create(User user) {
-        return userDao.insert(user) == 1;
-    }
-
-    public boolean update(User user) {
-        return userDao.updateByPrimaryKeySelective(user) == 1;
-    }
-
-    public boolean delete(Long id) {
-        return userDao.deleteByPrimaryKey(id) == 1;
-    }
+    boolean delete(Long id);
 }
---
+++ src/main/java/me/jinwenjie/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package me.jinwenjie.service.impl;
+
+import me.jinwenjie.model.User;
+import me.jinwenjie.dao.UserDao;
+import me.jinwenjie.model.UserExample;
+import me.jinwenjie.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override   
+    public List<User> list() {
+        UserExample example = new UserExample();
+        return userDao.selectByExample(example);
+    }
+
+    @Override
+    public User get(Long id) {
+        return userDao.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public boolean create(User user) {
+        return userDao.insert(user) == 1;
+    }
+
+    @Override
+    public boolean update(User user) {
+        return userDao.updateByPrimaryKeySelective(user) == 1;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return userDao.deleteByPrimaryKey(id) == 1;
+    }
+}
