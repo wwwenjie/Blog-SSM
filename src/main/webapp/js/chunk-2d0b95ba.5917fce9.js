8d9565f043c1a8272095ff204d490acb04ed5047
wenjie <jinwenjie@live.com>
2020-06-02 18:08:08 +0800

    chore(build): build to webapp

--- src/main/webapp/js/chunk-2d0b95ba.5917fce9.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b95ba"],{3337:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-container",{attrs:{id:"social",tag:"section"}})},o=[],c={name:"HomeSocial",components:{}},i=c,s=t("2877"),l=t("6544"),r=t.n(l),u=t("a523"),p=Object(s["a"])(i,a,o,!1,null,null,null);e["default"]=p.exports;r()(p,{VContainer:u["a"]})}}]);
-//# sourceMappingURL=chunk-2d0b95ba.5917fce9.js.map
